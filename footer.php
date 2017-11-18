9c2ffa9b820535c1dc7becf4532383f588538385
katigbanoma <leke4real@hotmail.com>
2017-11-18 14:42:19 +0100

    up

--- footer.php
+++ footer.php
@@ -6,21 +6,13 @@
       <i class="fa fa-twitter fa-stack-1x" style="color:red;"></i>
       </span> -->
       <div class="text-center" style="word-spacing:1em;">
-          <a href="https://www.facebook.com/Thewomenslawyer-747784002083724/" target="new">
             <i class="fa fa-facebook-square" style="font-size:28px;" aria-hidden="true"></i>          
-          </a>
           
-          <a href="https://www.instagram.com/womenslawyer_ng/" target="new">
             <i class="fa fa-instagram" style="font-size:28px;"></i>          
-          </a>
 
-          <a href="https://twitter.com/womenslawyer_ng" target="new">
             <i class="fa fa-twitter" style="font-size:28px;"></i>          
-          </a>
           
-          <a href="https://plus.google.com/u/0/104037967853574181673" target="new">
             <i class="fa fa-google-plus-square" style="font-size:28px;"></i>          
-          </a>
       </div>
       
         
